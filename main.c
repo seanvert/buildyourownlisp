db285d1b0bc1e33742c73ff60245e88270cd38bb
Sean Uchida <seankuchida@gmail.com>
2022-01-24 14:52:18 -0300

    eval eva_op adicionadas

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include "mpc.h"
 #include <editline/readline.h>
 
+long eval(mpc_ast_t* t);
+long eval_op(long x, char* op, long y);
+
 int main(int argc, char *argv[])
 {
   printf("Lispy version 0.0.0.0.1");
@@ -50,3 +53,27 @@ int main(int argc, char *argv[])
   return 0;
 }
 
+long eval(mpc_ast_t* t) {
+  if(strstr(t->tag, "numero")) {
+	return atoi(t->contents);
+  }
+  /* TODO não sei se este pedaço vai funcionar direito */
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+	x = eval_op(x, op, eval(t->children[i]));
+	i++;
+  }
+  return x;
+}
+
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
