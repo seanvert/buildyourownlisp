99d8160a7a5cdc05052f250322344ef2682d37e9
Sean Uchida <seankuchida@gmail.com>
2022-01-29 20:24:43 -0300

    erros: struct lval, enums com códigos de erros, lval_print

--- main.c
+++ main.c
@@ -3,9 +3,22 @@
 #include <string.h>
 #include "mpc.h"
 #include <editline/readline.h>
-
+/* enum com os possíveis tipos de lval */
+enum { LVAL_NUM, LVAL_ERR };
+/* enumeração com os possíveis códigos de erro */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+/* estrutura de dados lisp value */
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
 long eval(mpc_ast_t* t);
 long eval_op(long x, char* op, long y);
+lval lval_num(long x);
+lval lval_err(int x);
+void lval_print(lval v);
+void lval_println(lval v);
 
 int main(int argc, char *argv[])
 {
@@ -84,4 +97,44 @@ long eval(mpc_ast_t* q) {
 	j++;
   }
   return x;
+}
+
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v) {
+  switch (v.type) {
+  case LVAL_NUM: {
+	printf("%li", v.num);
+    break;
+  }
+  case LVAL_ERR: {
+    if (v.err == LERR_DIV_ZERO) {
+	  printf("Erro: divisão por zero\n");
+	}
+	if (v.err == LERR_BAD_OP) {
+	  printf("Erro: operador inválido\n");
+	}
+	if (v.err == LERR_BAD_NUM) {
+	  printf("Erro: número inválido\n");
+	}
+    break;
+  }
+  }
+}
+
+/* adiciona uma quebra de linha depois de um lval */
+void lval_println(lval v) {
+  lval_print(v);
+  putchar('\n');
 }
